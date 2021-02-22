79431f43d4254973b4d07306e16f83222e6a6ce7
Yuji Takeda <yujitakeda100@gmail.com>
2021-02-22 19:49:30 +0900

    1st commit

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ from linebot.models import TextSendMessage
 file = open('info.json','r')
 info = json.load(file)
 
+CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+def main():
+    USER_ID = info['USER_ID']
+    messages= TextSendMessage(text="おはよ〜 \n 朝だよ♥")
+    line_bot_api.push_message(USER_ID,messages=messages)
+    
+if __name__== "__main__":
+    main()
